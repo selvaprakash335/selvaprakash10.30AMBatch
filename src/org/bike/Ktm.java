e26f9903cdb5a99935f4a244be93ddae10685c5b
selvaprakash335 <selvaprakash335@gmail.com>
2021-07-03 11:58:02 +0530

    my first push

---
+++ src/org/bike/Ktm.java
@@ -0,0 +1,22 @@
+package org.bike;
+
+public class Ktm implements Bike {
+
+	@Override
+	public void cost() {
+		System.out.println("50,000");
+		
+	}
+
+	@Override
+	public void speed() {
+		System.out.println("125km");
+		
+	}
+	public static void main(String[] args) {
+		Ktm a=new Ktm();
+		a.speed();
+		a.cost();
+	}
+
+}
